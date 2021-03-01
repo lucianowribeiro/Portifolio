e126bfe8647c8ae96dd500dafd3dab17c1ea4a35
Luciano WRibeiro <lucianowribeiro@gmail.com>
2021-03-01 11:49:23 -0300

    :rotating_light: fix(lint): Fixed and validated eslint errors

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
-import styled from 'styled-components'
-import Capa from '../src/components/common/Capa'
-import Footer from '../src/components/common/Footer'
-import Container from '../src/components/foundation/Container'
-import Header from '../src/components/common/Header'
-import Projects from '../src/components/common/Projects'
+import React from 'react';
+import Capa from '../src/components/common/Capa';
+import Footer from '../src/components/common/Footer';
+import Container from '../src/components/foundation/Container';
+import Header from '../src/components/common/Header';
+import Projects from '../src/components/common/Projects';
 
 export default function Home() {
   return (
-      <Container>
-        <Header />
-        <Capa />
-        <Projects />
-        <Footer />
-      </Container>
-  )
+    <Container>
+      <Header />
+      <Capa />
+      <Projects />
+      <Footer />
+    </Container>
+  );
 }
