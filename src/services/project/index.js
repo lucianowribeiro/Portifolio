f10eaf559a46a888816469b19fb0a302cf95f37c
Luciano WRibeiro <lucianowribeiro@gmail.com>
2021-04-18 16:08:35 -0300

    :bugs: fix(test): Improve service project test

--- src/services/project/index.js
+++ src/services/project/index.js
@@ -1,9 +1,9 @@
 import CMSGraphCLient, { gql } from '../../infra/cms/CMSGraphClient';
 
 const projectService = {
-  async getProjects(type, query) {
+  async getProjects(type, query, CMSGraphCLientModule = CMSGraphCLient) {
     if (type === 'all') {
-      const projects = await CMSGraphCLient(gql`${query}`);
+      const projects = await CMSGraphCLientModule(gql`${query}`);
       return Object.values(projects.allProjectItems);
     } if (type === 'Item') {
       const { projectItem } = await CMSGraphCLient(gql`${query}`);
@@ -17,8 +17,8 @@ const projectService = {
     }
     return new Error('A valid type is required');
   },
-  async getPaths(query) {
-    const paths = await CMSGraphCLient(gql`${query}`);
+  async getPaths(query, CMSGraphCLientModule = CMSGraphCLient) {
+    const paths = await CMSGraphCLientModule(gql`${query}`);
     return Object.values(paths.allProjectItems).map((project) => ({
       params: { id: project.projectTitle },
     }));
